5400110300e08616cef39009de40ebe5d9b363e6
thakurji12034-pixel <thakurji.12034@gmail.com>
2026-08-29 14:52:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (12.17%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean check(int[] nums) {
+
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int next = (i + 1) % nums.length;
+
+            if (nums[i] > nums[next]) {
+                count++;
+            }
+        }
+
+        if (count <= 1) {
+            return true;
+        }
+
+        return false;
+    }
+}
