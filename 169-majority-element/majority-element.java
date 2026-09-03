d8b72c586cb640d36eb513528fbdbf3028b12da1
thakurji12034-pixel <thakurji.12034@gmail.com>
2026-09-03 23:05:45 +0530

    Time: 1 ms (99.90%) | Memory: 55.7 MB (46.50%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int answer = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {
+                answer = nums[i];
+            }
+            if (nums[i] == answer) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return answer;
+    }
+};
