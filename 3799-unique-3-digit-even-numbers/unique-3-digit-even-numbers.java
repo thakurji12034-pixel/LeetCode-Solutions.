3efd4952e8311ecf3cdc66716aeb97f6d21d121a
thakurji12034-pixel <thakurji.12034@gmail.com>
2026-09-18 21:00:19 +0530

    Time: 5 ms (68.75%) | Memory: 46.4 MB (30.26%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -0,0 +1,41 @@
+import java.util.HashSet;
+
+class Solution {
+    public int totalNumbers(int[] digits) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] == 0) {
+                continue;
+            }
+
+            for (int j = 0; j < digits.length; j++) {
+                if (j == i) {
+                    continue;
+                }
+
+             for (int k = 0; k < digits.length; k++) {
+                    if (k == i || k == j) {
+                        continue;
+                    }
+
+                    
+                    if (digits[k] % 2 != 0) {
+                        continue;
+                    }
+
+                   
+                    int number = digits[i] * 100
+                              + digits[j] * 10
+                              + digits[k];
+
+                   
+                    set.add(number);
+                }
+            }
+        }
+
+        return set.size();
+    }
+}
